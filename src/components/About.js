a8017d1492835e3e2832c27df29466d89795657e
valkaond <ondra.valka@gmail.com>
2023-07-17 10:36:10 +0200

    Creating about me template

--- src/components/About.js
+++ src/components/About.js
@@ -1,5 +1,20 @@
 function About (){
-    return <h2>This gonna be about me part</h2>
+    return (
+    <div id="about">
+        <div>
+            About Me
+        </div>
+        <div>
+            <h3>My name is Ondřej Válka</h3>
+            <p>
+                Lorem ipsum dolor sit amet, consectetuer adipiscing elit. Curabitur ligula sapien, pulvinar a vestibulum quis, facilisis vel sapien. Nulla pulvinar eleifend sem. Aliquam erat volutpat. Vivamus porttitor turpis ac leo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Fusce aliquam vestibulum ipsum. 
+            </p>
+            <p>
+                Curabitur vitae diam non enim vestibulum interdum. Pellentesque pretium lectus id turpis. Itaque earum rerum hic tenetur a sapiente delectus, ut aut reiciendis voluptatibus maiores alias consequatur aut perferendis doloribus asperiores repellat. Nunc auctor. Class aptent taciti sociosqu ad litora torquent per conubia nostra, per inceptos hymenaeos. Nullam faucibus mi quis velit. Mauris dictum facilisis augue. Praesent dapibus. Sed ac dolor sit amet purus malesuada congue. 
+            </p>
+        </div>
+    </div>
+    )
 }
 
 export default About;
