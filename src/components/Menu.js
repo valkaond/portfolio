323a669ff44f286d004007a425919e03e729298f
valkaond <ondra.valka@gmail.com>
2023-07-17 21:29:39 +0200

    Menu rework (ul li) and new styling

--- src/components/Menu.js
+++ src/components/Menu.js
@@ -6,10 +6,10 @@ function Menu(){
         <div>
             <div className="menu">
                 <ul>
-                    <li><a href="/#">Home</a></li>
-                    <li><a href="/#about">About</a></li>
-                    <li><a href="/#projects">Projects</a></li>
-                    <li><a href="/#contact">Contact</a></li>
+                    <li><a className="menuLink" href="/#">Home</a></li>
+                    <li><a className="menuLink" href="/#about">About</a></li>
+                    <li><a className="menuLink" href="/#projects">Projects</a></li>
+                    <li><a className="menuLink" href="/#contact">Contact</a></li>
                 </ul>
             </div>
         </div>
