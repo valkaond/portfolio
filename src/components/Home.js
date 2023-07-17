0be07f6c2b96e82afd9dd41c664faf6ff8e0cf24
valkaond <ondra.valka@gmail.com>
2023-07-17 12:49:07 +0200

    Creating home template

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,24 @@
+import React from 'react';
+import Typewriter from "typewriter-effect";
+import '../App.css';
 function Home(){
-   return <h1>Im valkaond</h1>
+   return (
+      <div id="home">
+         <Typewriter
+            options={{
+               strings: [
+                  "I'm Ondřej Válka",
+                  "I'm student at FIT CTU in Prague"
+               ],
+               delay: 100,
+               pauseFor: 1750,
+               autoStart: true,
+               loop: true
+            }
+            }
+         />
+      </div>
+   );
 }
 
 export default Home;
