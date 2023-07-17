9f5350cf6e26baa64b80e17c6c376bfb901e3ecb
valkaond <ondra.valka@gmail.com>
2023-07-17 13:22:20 +0200

    Creating projects template

--- src/components/Projects.js
+++ src/components/Projects.js
@@ -1,5 +1,37 @@
 function Projects(){
-    return <h2>Projects part</h2>
+    return (
+        <div id="projects">
+            <div>
+                Projects
+                <div>
+                    BeachStats
+                    {/* Add image*/}
+                    <div>
+                        Statistical application for beach volleyball
+                    </div>
+                    <div>
+                        My first project, was part of my high school final exam. 
+                    </div>
+                    <div onClick={()=>window.open("https://beachstats.netlify.app/")}>
+                        Try BeachStats
+                    </div>    
+                </div>
+                <div>
+                    Weather App
+                    {/* Add image*/}
+                    <div>
+                        App to find out the current weather in different locations
+                    </div>
+                    <div>
+                        On this project I wanted to learn how to work with the API
+                    </div>
+                    <div onClick={()=>window.open("https://weather-app-valkaond.netlify.app/")}>
+                        Try WeatherApp
+                    </div>  
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default Projects;
