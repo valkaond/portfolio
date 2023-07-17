3db9f2f801420f53491f7a30fff2606d8c4e725c
valkaond <ondra.valka@gmail.com>
2023-07-17 11:16:53 +0200

    Creating contact template

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,5 +1,19 @@
+import React from "react";
 function Contact (){
-    return <h2>Contact me part</h2>
+    return (
+    <div id="contact">
+        <div>
+            Contact
+        </div>
+        <div onClick={()=>window.open("https://www.linkedin.com/in/valkaond/")}>
+            Visit my LinkedIn
+        </div>
+        <div>
+            Send me an
+            <a href="mailto:ondra.valka@gmail.com"> Email</a>
+        </div>
+    </div>
+    )
 }
 
 export default Contact;
